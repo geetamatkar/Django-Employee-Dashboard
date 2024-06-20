4226dc9d2eca9bf06be5b3099e0af45099fc7e18
geetamatkar <geetamatkar1@gmail.com>
2024-06-20 10:59:07 -0500

    # send data from views to template

--- myapp/views.py
+++ myapp/views.py
@@ -4,9 +4,30 @@ import datetime
 
 def home(request):
     date=datetime.datetime.now()
+    isActive = True
+    name = 'IIT'
+    list_of_programs = [
+        'Web Dev',
+        'Algorithms',
+        'Programming',
+        'OOP'
+    ]
+    student = {
+        'student_name':"John",
+        'student_address':"Chicago"
+    }
+
+    data = {
+        'date':date,
+        'isActive' : isActive,
+        'name' : name,
+        'list_of_programs' : list_of_programs,
+        'student_data':student,
+    }
+
     print("Home function from view")
     #return HttpResponse("<h1>This is index page</h1>"+str(date))
-    return render(request, "home.html", {})
+    return render(request, "home.html", data)
 
 def about(request):
     #return HttpResponse("<h1>This is about page</h1>")
