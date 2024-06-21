cb239150bb02bd9b8f85e8943add95c67294cc98
geetamatkar <geetamatkar1@gmail.com>
2024-06-21 09:07:03 -0500

    # Update employee details

--- emp/views.py
+++ emp/views.py
@@ -45,3 +45,37 @@ def delete_emp(request,emp_id):
     emp=Emp.objects.get(pk=emp_id)
     emp.delete()
     return redirect("/emp/home/")
+
+def update_emp(request,emp_id):
+    emp=Emp.objects.get(pk=emp_id)
+    return render(request, 'emp/update_emp.html', {
+        'emp':emp
+    })
+
+def do_update_emp(request,emp_id):
+    print("To update emp_id: ", emp_id)
+    if request.method=='POST':
+        print("inside if")
+        #data fetch
+        emp_name=request.POST.get("emp_name")
+        emp_id_temp=request.POST.get("emp_id")
+        emp_phone=request.POST.get("emp_phone")
+        emp_address=request.POST.get("emp_address")
+        emp_working=request.POST.get("emp_working")
+        emp_department=request.POST.get("emp_department")
+
+        print("Name of employee: ", emp_name)
+
+        e=Emp.objects.get(pk=emp_id)
+        e.name=emp_name
+        e.emp_id=emp_id_temp
+        e.phone=emp_phone
+        e.address=emp_address
+        e.department=emp_department
+        if emp_working==None:
+            e.working=False
+        else:
+            e.working=True
+
+        e.save()
+    return redirect ("/emp/home/")
