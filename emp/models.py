ca573f046d204a3d8ff8b510cbe1f5d93336e7b5
geetamatkar <geetamatkar1@gmail.com>
2024-06-20 19:39:41 -0500

    # Storing form data to database

--- emp/models.py
+++ emp/models.py
@@ -5,7 +5,7 @@ class Emp(models.Model):
     name=models.CharField(max_length=200)
     emp_id=models.CharField(max_length=200)
     phone=models.CharField(max_length=10)
-    city=models.CharField(max_length=150)
+    address=models.CharField(max_length=150)
     working=models.BooleanField(default=True)
     department=models.CharField(max_length=10)
     
