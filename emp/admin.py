f0b34893589260bc6616420848427a276e14d2f1
geetamatkar <geetamatkar1@gmail.com>
2024-06-23 09:18:28 -0500

    # Django admin, admin customization

--- emp/admin.py
+++ emp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Emp
 # Register your models here.
+class EmpAdmin(admin.ModelAdmin):
+    list_display=('name','working', 'emp_id')
+    #list_editable=('working', 'phone')
+    search_fields=('name', 'address')
+    list_filter=('address','working')
 
-admin.site.register(Emp)
+admin.site.register(Emp, EmpAdmin)
--- emp/models.py
+++ emp/models.py
@@ -8,4 +8,7 @@ class Emp(models.Model):
     address=models.CharField(max_length=150)
     working=models.BooleanField(default=True)
     department=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
     
